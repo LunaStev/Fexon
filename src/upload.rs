87f98dff590aeada658ad90d2f60a424ab530131
LunaStev <youngjae681@gmail.com>
2024-11-23 18:05:53 +0900

    remove uuid and add rand

--- src/upload.rs
+++ src/upload.rs
@@ -1,14 +1,25 @@
 use actix_multipart::Multipart;
 use actix_web::{HttpResponse, Result};
 use futures::stream::StreamExt;
-use futures::SinkExt;
+use rand::Rng;
 use std::fs::{create_dir_all, File};
 use std::io::Write;
 use std::path::{Path, PathBuf};
-use uuid::Uuid;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 const MAX_FILE_SIZE: u64 = 10 * 1024 * 1024; // 10MB
 const ALLOWED_MIME_TYPES: [&str; 2] = ["image/jpeg", "image/png"];
+
+fn generate_unique_filename() -> String {
+    let start = SystemTime::now();
+    let duration = start.duration_since(UNIX_EPOCH).expect("Time went backwards");
+    let timestamp = duration.as_secs();
+
+    let mut rng = rand::thread_rng();
+    let random_suffix: u64 = rng.gen_range(1000..9999);
+
+    format!("{}_{:04}", timestamp, random_suffix)
+}
 
 pub async fn upload_file(mut payload: Multipart) -> Result<HttpResponse> {
     let temp_dir = "uploads";
@@ -23,7 +34,7 @@ pub async fn upload_file(mut payload: Multipart) -> Result<HttpResponse> {
         }
 
         let mut total_size = 0;
-        let file_name = Uuid::new_v4().to_string();
+        let file_name = generate_unique_filename();
         let mut file_path = PathBuf::from(temp_dir);
         file_path.push(file_name);
 
